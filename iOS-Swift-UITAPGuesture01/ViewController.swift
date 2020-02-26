d353ca86d32f850fdee8aa36f6396dec15b6e5a1
Pooya Hatami <am.hatami@gmail.com>
2020-02-26 14:24:44 -0800

    iOS UITapGestureRecognizer pass parameter Tutorial !

--- iOS-Swift-UITAPGuesture01/ViewController.swift
+++ iOS-Swift-UITAPGuesture01/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var passIntAct: UIButton!
+    @IBOutlet weak var passStringAct: UIButton!
+    @IBOutlet weak var passFloatAct: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        passIntAct.tag = 10
+        passIntAct.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.passIntAct(gesture:))))
+        
+        
+        
+        
+        let strTapped = MyTapGesture.init(target: self, action: #selector(self.passStringAct(recognizer:)))
+        strTapped.stringValue = "YES String DONE"
+        strTapped.floatValue = 21.0
+        passStringAct.addGestureRecognizer(strTapped)
+        
+        let floatTapped = MyTapGesture.init(target: self, action: #selector(self.passFloatAct(recognizer:)))
+        floatTapped.stringValue = "YES Float DONE"
+        floatTapped.floatValue = 28.0
+        passFloatAct.addGestureRecognizer(floatTapped)
+        
+        
     }
 
+    @objc func passStringAct(recognizer: MyTapGesture) {
+        print(recognizer.stringValue)
+        print(recognizer.floatValue)
+    }
 
+    @objc func passFloatAct(recognizer: MyTapGesture) {
+        print(recognizer.stringValue)
+        print(recognizer.floatValue)
+    }
+    
+    @IBAction func passIntAct(gesture: UITapGestureRecognizer) {
+        print("Pass Int = \(passIntAct.tag)")
+    }
+    
 }
 
+
+class MyTapGesture: UITapGestureRecognizer {
+    var floatValue = 15.5
+    var stringValue = "text text"
+}
